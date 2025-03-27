d2ae176e0792293194be8dd4ca13a9ca0f8503b0
vabisha <139101295+vabisha@users.noreply.github.com>
2025-03-27 17:06:32 +0530

    Add files via upload

---
+++ Day01_practical/second.asm
@@ -0,0 +1,17 @@
+.model small
+.stack 100h
+.data
+  msg db "Hello ",10,"How are you? $"
+.code
+start:
+  mov ax,@data
+  mov ds,ax
+  
+  mov dx, offset msg
+  mov ah, 09h
+  int 21h
+  
+  mov ax,4c00h
+  int 21h
+  
+end start
